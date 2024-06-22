5ae5fe5b536e35605f1d771e6b3b3e7563b8d35c
altynbeq <starknbek@gmail.com>
2024-06-22 12:43:10 +0500

    n

--- src/pages/General.jsx
+++ src/pages/General.jsx
@@ -18,7 +18,7 @@ const General = () => {
       </div>
       <div className="flex  gap-4 flex-col md:flex-row  justify-center">
         <SalesDouble />
-        <div className=' flex justify-center align-center flex-col  w-[80%] md:w-[30%]'>
+        <div className=' flex justify-center align-center flex-col  w-[80%] md:w-[40%]'>
           <MonthlyRevenueBars />
           <SecondRowPie />
         </div>
