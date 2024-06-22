d558dcef16232bb0057247490185fcaf9981f995
altynbeq <starknbek@gmail.com>
2024-06-22 15:20:20 +0500

    v4

--- src/components/Finance/RevenueByMonthsStacked.jsx
+++ src/components/Finance/RevenueByMonthsStacked.jsx
@@ -5,9 +5,9 @@ import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '.
 
 const RevenueByMonthsStacked = () => {
   return (
-    <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-[90%] md:w-[50%]">
+    <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-[50%] md:w-[50%]">
       <div className="flex justify-between items-center gap-2 mb-10">
-          <p className="text-xl font-semibold">Доход за V3</p>
+          <p className="text-xl font-semibold">Доход за V4</p>
           <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
               <span>
