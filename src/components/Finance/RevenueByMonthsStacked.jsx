ded1a0cf0a15f4d8c6a7f1eb0bec7eeb3993aca1
altynbeq <starknbek@gmail.com>
2024-06-22 15:30:52 +0500

    v5

--- src/components/Finance/RevenueByMonthsStacked.jsx
+++ src/components/Finance/RevenueByMonthsStacked.jsx
@@ -7,7 +7,7 @@ const RevenueByMonthsStacked = () => {
   return (
     <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-[90%] md:w-[50%]">
       <div className="flex justify-between items-center gap-2 mb-10">
-          <p className="text-xl font-semibold">Доход за V4</p>
+          <p className="text-xl font-semibold">Доход за V5</p>
           <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
               <span>
