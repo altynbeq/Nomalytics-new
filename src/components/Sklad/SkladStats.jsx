5a222260abb814bf3dd5184b5fa2911caafe7e7b
altynbeq <starknbek@gmail.com>
2024-06-23 15:09:06 +0500

    F6

--- src/components/Sklad/SkladStats.jsx
+++ src/components/Sklad/SkladStats.jsx
@@ -6,7 +6,7 @@ import { useStateContext } from '../../contexts/ContextProvider';
 const SkladStats = () => {
   const { currentColor, currentMode } = useStateContext();
   return (
-    <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg  p-4 rounded-2xl w-[90%] md:w-fit    xs:mr-4 xs:ml-4 md:mr-0 ">
+    <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg  p-4 rounded-2xl w-[80%] md:w-fit    md:mr-0 ">
           <div className="flex justify-between">
             <p className="font-semibold text-xl xs:text-sm xs:ml-12 xs:mt-6 xl:ml-6 xl:mt-4 md:mt-4 md:ml-6 sm:ml-12 sm:mt-8 ">За неделю</p>
             <div className="flex items-center gap-4">
