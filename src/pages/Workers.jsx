dce9e73d9f52ff327994a93a379a3b2379c8ba28
altynbeq <starknbek@gmail.com>
2024-06-26 01:30:34 +0500

    sklad

--- src/pages/Workers.jsx
+++ src/pages/Workers.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { floristList, cusSupportList, tasksList } from '../data/workersData';
 import { floristsStats, callCenterStats } from '../data/workersData';
 
-import { WorkersList, Tasks, WorkerStats } from '../components/Workers';
+import { FloristList, SupportList, Tasks, WorkerStats } from '../components/Workers';
 import { useStateContext } from '../contexts/ContextProvider';
 import { fetchLeads } from '../methods/getLeads';
 
@@ -19,8 +19,8 @@ const Workers = () => {
     <div className='mt-12 flex flex-col justify-center align-center '>
       <div className="flex flex-wrap lg:flex-nowrap justify-center flex-col p-10">
         <div className="flex flex-wrap lg:flex-nowrap gap-5 justify-center  ">
-          <WorkersList data={floristList} title="Список флористов" />
-          <WorkersList data={cusSupportList} title="Список консультантов" />
+          <FloristList data={floristList} title="Список флористов" />
+          <SupportList data={cusSupportList} title="Список консультантов" />
           <Tasks data={tasksList} title="Список задач" />
         </div>
         <div className="flex flex-wrap  gap-5 justify-center w-full  ">
